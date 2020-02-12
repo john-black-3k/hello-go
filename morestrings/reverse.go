45d4bfd7f0bb594451ffc2e8255fa6ac9b75b845
John <john.black.3k@gmail.com>
2020-02-12 19:44:54 +0200

    Removed newline

--- morestrings/reverse.go
+++ morestrings/reverse.go
@@ -1,6 +1,5 @@
 // Package morestrings implements additional functions to manipulate UTF-8
 // encoded strings, beyond what is provided in the standard "strings" package.
-
 package morestrings
 
 // ReverseRunes returns its argument string reversed rune-wise left to right.
